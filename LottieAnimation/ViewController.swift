8a12e33d0f6272edfab5bfb5a247651d1fb5073b
SeongMin-K <eo1tks3@naver.com>
2021-09-24 17:10:32 +0900

    Initial Commit

---
+++ LottieAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottieAnimation
+//
+//  Created by SeongMinK on 2021/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
